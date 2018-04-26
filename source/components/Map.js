9c4d7e516f772bca69927ad24085af606e239ff4
Alec Lindemann <alec.lindemann@gmail.com>
2018-04-26 18:44:25 -0500

    Add files via upload

--- source/components/Map.js
+++ source/components/Map.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import MapView from 'react-native-maps';
+import HomeCard from './HomeCard.js';
+import { connect } from 'react-redux';
 
-export default class Map extends React.Component {
+class Map extends React.Component {
     static navigationOptions = {
         title: 'Map',
         header: null,
@@ -11,11 +13,46 @@ export default class Map extends React.Component {
     constructor() {
         super();
     }
+    testFunc(deal, img, title, color, descrip) {
+      this.props.navigation.navigate('HomeDetail', {deal: deal, img: img, title: title, color: color, description: descrip})
+    }
+    test2Func(){
+      console.log(this.props.dealList)
+    }
 
     render() {
         return(
             <View style={styles.container}>
-                <Text>Map</Text>
+                <MapView style={styles.map}
+                  region={{
+                    latitude: 40.1080007,
+                    longitude: -88.2224638,
+                    latitudeDelta: .05,
+                    longitudeDelta: .05,
+                  }}
+                  provider="google"
+                  showsUserLocation={true}
+                  showsMyLocationButton={true}>
+                  <MapView.Marker
+                    coordinate={{
+                      latitude: 40.1080007,
+                      longitude: -88.2224638,
+                    }}
+                    title={'Concert'}
+                    description={'Shitty Orchestra'}
+                    onPress={() => this.testFunc("100% off", require('../assets/fightingillinibasketball.jpg'), "event2", '#555555', 'description')}
+                  />
+                  <MapView.Marker
+                    coordinate={{
+                      latitude: 40.0962534,
+                      longitude: -88.2358917,
+                    }}
+                    title={'Basketball Game'}
+                    description={'Illinois vs Kansas State'}
+                    onPress={() => this.testFunc("100% off", require('../assets/fightingillinibasketball.jpg'), "event1", '#555555', 'description')}
+                  />
+
+                </MapView>
             </View>
         );
 
@@ -29,4 +66,16 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
     },
+    map: {
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      bottom: 0,
+      right: 0
+    }
 });
+const mapStateToProps = (state) => ({
+    dealList: state.deal.dealList
+});
+export default connect(mapStateToProps)(Map);
+//export default
